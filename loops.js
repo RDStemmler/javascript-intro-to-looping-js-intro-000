8078ced3f8849216faecbcf3f811505e0d3d0663
Rob <rdstemmler@gmail.com>
2017-10-28 22:12:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,3 +15,13 @@ function whileLoop(n) {
   }
   return 'done'
 }
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  array.pop()
+} while (array.length > 0 && maybeTrue());
+
+return array;
